5fafe6b254741d1173e560df7782d5133d3d667c
EasyCanadianGamer <eyadi2005@gmail.com>
2026-02-24 07:15:10 -0500

    added your mm

--- src/app/surah/[surahNumber]/page.tsx
+++ src/app/surah/[surahNumber]/page.tsx
@@ -1,9 +1,5 @@
 import SurahDetail from '../../../components/SurahDetail'
 
-export function generateStaticParams() {
-  return Array.from({ length: 114 }, (_, i) => ({ surahNumber: String(i + 1) }))
-}
-
 export default function SurahPage() {
   return <SurahDetail />
 }
